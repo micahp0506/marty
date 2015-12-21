192f31378c301ba8fa17c1fba384e5f95d99201f
Micah Wells <Micah@Micahs-MacBook-Pro.local>
2015-12-21 10:41:35 -0600

    Leadeboard working

--- js/controllers/leaderboard.js
+++ js/controllers/leaderboard.js
@@ -1,6 +1,7 @@
 app.controller("leaderboard", ["$scope", "$location", "$firebaseArray", "$filter",
 	function($scope, $location, $firebaseArray, $filter) {
 
+		// removeFromDOM(canvas);
 		// Firebase highscores ref
     	var scoresRef = new Firebase("https://marty.firebaseio.com/scores/");
     	// Firebase array for filtering by partial
